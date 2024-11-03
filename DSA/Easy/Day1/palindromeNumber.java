1bf9e33ed15a141ae1baba41c1c2dc75c287b338
Rahul Kujur <rahulkujur31@gmail.com>
2024-11-03 21:00:16 +0530

    Day 3

--- DSA/Easy/Day1/palindromeNumber.java
+++ DSA/Easy/Day1/palindromeNumber.java
@@ -1,7 +1,30 @@
 package DSA.Easy.Day1;
 
-public class palindromeNumber{
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int original = x;
+        int reversed = 0;
+        while (x > 0) {
+            int lastDigit = x % 10;
+            // Check for overflow/underflow before updating reversed
+            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && lastDigit > 7)) {
+                return false;  // Overflow case for large inputs
+            }
+            reversed = reversed * 10 + lastDigit;
+            x /= 10;
+        }
+        return original == reversed;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello Wrold!");
+        PalindromeNumber checker = new PalindromeNumber();
+        
+        int number = 121;  // Example number
+        System.out.println(number + " is palindrome: " + checker.isPalindrome(number));  
+        // Output: true
     }
 }
--- DSA/Easy/Day2/RomanToInteger.java
+++ DSA/Easy/Day2/RomanToInteger.java
@@ -1,11 +1,40 @@
 package DSA.Easy.Day2;
 
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RomanToInteger {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner();
-        int s = sc.nextLine();
+    public int romanToInt(String s) {
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int result = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            int current = romanMap.get(s.charAt(i));
+            if (i < s.length() - 1 && current < romanMap.get(s.charAt(i + 1))) {
+                result -= current;
+            } else {
+                result += current;
+            }
+        }
+        
+        return result;
     }
 
+    public static void main(String[] args) {
+        RomanToInteger converter = new RomanToInteger();
+        
+        String roman = "MCMXCIV";  
+        int result = converter.romanToInt(roman);
+        
+        System.out.println("The integer value of " + roman + " is " + result);  // Output: 1994
+    }
 }
+
